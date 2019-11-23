f7298a2fdbd84af05eef8efdcd8eedc80f80acd0
Caroline Chan <carolinechanccc@gmail.com>
2019-11-23 09:07:35 +0000

    Add notes

--- pdf-playground/watermark.py
+++ pdf-playground/watermark.py
@@ -1,13 +1,14 @@
 import PyPDF2
 
-template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
-watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
-output = PyPDF2.PdfFileWriter()
+template = PyPDF2.PdfFileReader(open('super.pdf', 'rb')) # read and open the PDF to process 
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb')) # read and open the watermark PDF
+output = PyPDF2.PdfFileWriter() # write the output and combine the PDFs
 
-for i in range(template.getNumPages()):
-    page = template.getPage(i)
-    page.mergePage(watermark.getPage(0))
-    output.addPage(page)
+# loop through the pages and add the watermark
+for i in range(template.getNumPages()): # loop through the range number of pages the template has
+    page = template.getPage(i) # page object - get each page
+    page.mergePage(watermark.getPage(0)) # get the one watermark page and merge the watermark onto the page object
+    output.addPage(page) # add the merged page
 
-    with open('watermarked_output.pdf', 'wb') as file:
+    with open('watermarked_output.pdf', 'wb') as file: # write the watermarked output
         output.write(file)
